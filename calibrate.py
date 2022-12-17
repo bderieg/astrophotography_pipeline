7e0a3fb7651513f725ec1082e7738e8e0cfa7dbd
Ben Derieg <bderieg@deriegfamily.com>
2022-12-17 11:23:29 -0700

    Darks can combine now

--- calibrate.py
+++ calibrate.py
@@ -1,12 +1,31 @@
 import os
+from astropy.io import fits
+import matplotlib.pyplot as plt
+import numpy as np
 
 def combine_darks(path):
-    red_dark = []
-    green_dark = []
-    blue_dark = []
+    red_dark = np.empty(0)
+    green_dark = np.empty(0)
+    blue_dark = np.empty(0)
     for (p, d, files) in os.walk(path):
         for filename in files:
             if filename.startswith('dark') and filename.endswith('.fits'):
-                if 'G' in filename:
-                    if len(green_dark) < 1:
-                        # green_dark = 
+                data = np.asarray(fits.open(path + "/" + filename)[0].data)
+                if 'R' in filename:
+                    if red_dark.size < 1:
+                        red_dark = data
+                    else:
+                        red_dark = (red_dark + data)/2
+                elif 'G' in filename:
+                    if green_dark.size < 1:
+                        green_dark = data
+                    else:
+                        green_dark = (green_dark + data)/2
+                elif 'B' in filename:
+                    if blue_dark.size < 1:
+                        blue_dark = data
+                    else:
+                        blue_dark = (blue_dark + data)/2
+    fits.writeto(path + '/' + 'full_dark_R.fits', red_dark)
+    fits.writeto(path + '/' + 'full_dark_G.fits', green_dark)
+    fits.writeto(path + '/' + 'full_dark_B.fits', blue_dark)
--- convert_files.py
+++ convert_files.py
@@ -1,24 +1,23 @@
 from cr2fits import cr2fits
 import os
+from tqdm.auto import tqdm
 
 def convert_all(path):
     for (p, d, files) in os.walk(path):
+        # Initialize progress bar
+        progress_bar = tqdm(range(len(files)+1), desc="Converting all files to .fits", leave=False)
         for filename in files:
             if filename.endswith('.CR2'): 
-                # TODO: Add progress bar
-                print("Converting \'" + filename + "\' to .FITS")
-
                 for color in range(3):
                     # Read in CR2 image
                     inpfile = cr2fits.cr2fits(path + "/" + filename, color)
                     inpfile.read_cr2()
                     ## Convert to numpy array
                     full_img = inpfile.read_pbm(inpfile.pbm_bytes)
-                    
                     # Get individual color array
                     im_color = inpfile.get_color(full_img, inpfile.colorInput)
-                    
                     # Output to fits
                     fits_color = inpfile.create_fits(im_color)
                     dest = inpfile._generate_destination(inpfile.filename, inpfile.colorInput)
                     inpfile.write_fits(fits_color, dest)
+            progress_bar.update(1)
